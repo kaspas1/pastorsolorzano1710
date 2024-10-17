ad8ee1ab65d5cfba11a6fdc72f4a3be90733a2af
unknown <UFG@LDI-07.ufg.local>
2024-10-17 08:02:05 -0600

    Aplicacion usando backend y frontend

---
+++ proyect-vue-dops/vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+/*module.exports = defineConfig({
+  transpileDependencies: true
+})*/
+
+module.exports={
+  pwa:{
+  name:"Proyecto de Disraely Omar Pastor Solórzano",
+  themeColor: "#FFd100",
+  msTileColor: "#000000",
+  appleMobileWebAppCapable: "yes",
+  appleMobileWebAppStatusBarStyle: "Black"
+  }
+}
